74da74a13e4aca8c8cf60ea3fb90f77cd884332d
Ed Summers <ehs@pobox.com>
2020-01-17 16:25:55 -0500

    empty shells of new pages from new sitemap

---
+++ src/pages/news/blog.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    AADHUM Content!
+  </Layout>
+)
+
+export default IndexPage
