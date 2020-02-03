f59816102115ceb599b1b4bfb7a14f08e1d09852
Ed Summers <ehs@pobox.com>
2020-02-03 13:31:34 -0500

    Almost working responsive menu.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,16 +4,46 @@ import React, { useState } from "react"
 import "./navbar.css"
 
 const NavBar = ({ menuLinks }) => {
+  const [display, toggleDisplay] = useState(false)
   return (
     <nav>
-      {menuLinks.map(cat => (
-        <MenuList key={cat.name} name={cat.name} links={cat.links} />
-      ))}
+      <a id="icon" href="#" onClick={() => toggleDisplay(!display)}> Menu &#9776;</a>
+      {menuLinks.map(cat => {
+        return (
+          <MenuList key={cat.name} name={cat.name} links={cat.links} defaultDisplay={display} />
+        )
+      })}
     </nav>
   )
 }
 
-const MenuList = ({ name, links }) => {
+const MenuList = ({ name, links, defaultDisplay }) => {
+  console.log(defaultDisplay)
+  const [display, setDisplay] = useState(false)
+  const style = {
+    display: display || defaultDisplay ? 'block' : 'none',
+    transition: 'max-height 0.2s ease-out'
+  }
+  return (
+    <div
+      className="menu-list"
+      onMouseEnter={() => setDisplay(true)}
+      onMouseLeave={() => setDisplay(false)}>
+      <span style={style} className="menu-name">{name}</span>
+      <ul style={style} className="links">
+        {links.map(link => (
+          <li key={link.name}>
+            <Link to={link.href} activeClassName="active">
+              {link.name}
+              </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const MobileMenuList = ({ name, links }) => {
   const [display, setDisplay] = useState(false)
   const style = {
     display: display ? 'block' : 'none',
@@ -37,6 +67,7 @@ const MenuList = ({ name, links }) => {
     </div>
   )
 }
+
 
 NavBar.propTypes = {
   menuLinks: PropTypes.array,
