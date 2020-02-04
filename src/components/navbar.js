167805f62ee4dbf7291ca077e79d2678e6261e8e
Ed Summers <ehs@pobox.com>
2020-02-04 09:26:37 -0500

    Redesigned to be responsive.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,33 +4,41 @@ import React, { useState } from "react"
 import "./navbar.css"
 
 const NavBar = ({ menuLinks }) => {
-  const [display, toggleDisplay] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <nav>
-      <a id="icon" href="#" onClick={() => toggleDisplay(!display)}> Menu &#9776;</a>
+      <a className="icon" href="#" onClick={() => setMenuOpen(!menuOpen)}> Menu &#9776;</a>
       {menuLinks.map(cat => {
         return (
-          <MenuList key={cat.name} name={cat.name} links={cat.links} defaultDisplay={display} />
+          <MenuList key={cat.name} name={cat.name} links={cat.links} menuOpen={menuOpen} />
         )
       })}
     </nav>
   )
 }
 
-const MenuList = ({ name, links, defaultDisplay }) => {
-  console.log(defaultDisplay)
-  const [display, setDisplay] = useState(false)
-  const style = {
-    display: display || defaultDisplay ? 'block' : 'none',
-    transition: 'max-height 0.2s ease-out'
+const MenuList = ({ name, links, menuOpen }) => {
+  const [linksActive, setLinksActive] = useState(false)
+
+  let nameOpen = ''
+  let linksOpen = ''
+
+  if (menuOpen) {
+    nameOpen = 'active'
+    linksOpen = 'active'
   }
+
+  if (linksActive) {
+    linksOpen = 'active'
+  }
+
   return (
     <div
-      className="menu-list"
-      onMouseEnter={() => setDisplay(true)}
-      onMouseLeave={() => setDisplay(false)}>
-      <span style={style} className="menu-name">{name}</span>
-      <ul style={style} className="links">
+      className={`menu-list ${nameOpen}`}
+      onMouseEnter={() => setLinksActive(true)}
+      onMouseLeave={() => setLinksActive(false)}>
+      <span className={`menu-name ${nameOpen}`}>{name}</span>
+      <ul className={`links ${linksOpen}`}>
         {links.map(link => (
           <li key={link.name}>
             <Link to={link.href} activeClassName="active">
@@ -42,32 +50,6 @@ const MenuList = ({ name, links, defaultDisplay }) => {
     </div>
   )
 }
-
-const MobileMenuList = ({ name, links }) => {
-  const [display, setDisplay] = useState(false)
-  const style = {
-    display: display ? 'block' : 'none',
-    transition: 'max-height 0.2s ease-out'
-  }
-  return (
-    <div
-      className="menu-list"
-      onMouseEnter={() => setDisplay(true)}
-      onMouseLeave={() => setDisplay(false)}>
-      <span className="menu-name">{name}</span>
-      <ul style={style} className="links">
-        {links.map(link => (
-          <li key={link.name}>
-            <Link to={link.href} activeClassName="active">
-              {link.name}
-              </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
 
 NavBar.propTypes = {
   menuLinks: PropTypes.array,
